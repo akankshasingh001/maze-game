1f33ca41e6abcbe7cc2e1a832cd09ae8cdc2a41c
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-15 12:18:20 -0400

    remove functionality for random shapes

--- index.js
+++ index.js
@@ -25,21 +25,3 @@ const walls = [
   Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
 ];
 World.add(world, walls);
-//20 rectangle/circle shape inside walls
-for (let i = 0; i < 20; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world,
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: 'green'
-        }
-      })
-    );
-  }
-}
