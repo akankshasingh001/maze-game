b9b9a88a12d892e3a01f2dd6539cf112e2b9aea9
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-15 19:14:49 -0400

    added keyboard controls

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 
 const width = 600;
 const height = 600;
@@ -171,26 +171,27 @@ World.add(world, ball);
 
 //Handling Keypress
 document.addEventListener('keydown', event => {
+  const { x, y } = ball.velocity;
   switch (event.keyCode) {
     //w or up arrow
     case 87:
     case 38:
-      console.log('up');
+      Body.setVelocity(ball, { x, y: y - 5 });
       break;
     //d or right arrow
     case 68:
     case 39:
-      console.log('right');
+      Body.setVelocity(ball, { x: x + 5, y });
       break;
     //s or down arrow
     case 83:
     case 40:
-      console.log('down');
+      Body.setVelocity(ball, { x, y: y + 5 });
       break;
     //a or left arrow
     case 65:
     case 37:
-      console.log('left');
+      Body.setVelocity(ball, { x: x - 5, y });
       break;
   }
 });
