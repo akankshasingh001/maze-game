1fa524cc0921e4ee4adb97f6dc3f5e799ca1e944
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-15 12:25:40 -0400

    added logic to create border based on width & height variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -19,9 +20,9 @@ Runner.run(Runner.create(), engine);
 
 //walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
