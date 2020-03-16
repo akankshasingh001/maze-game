7ef7fabd359c1714c5685d72e8825fa670e941b1
Akanksha Singh <singhakanksha001@gmail.com>
2020-03-16 19:57:55 -0400

    added css to buttons & logic to hide button

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-document.querySelector('.startGame').addEventListener('click', () => {
-  startGame(3, 4);
+document.querySelector('#btnEasy').addEventListener('click', () => {
+  disableGameLevel();
+  startGame(5, 5);
 });
+document.querySelector('#btnMedium').addEventListener('click', () => {
+  disableGameLevel();
+  startGame(10, 10);
+});
+document.querySelector('#btnHard').addEventListener('click', () => {
+  disableGameLevel();
+  startGame(20, 20);
+});
+
+disableGameLevel = () => {
+  document.querySelector('.gameLevel').classList.add('hidden');
+};
+// document.querySelector('.startGame').addEventListener('click', () => {
+//   startGame(3, 4);
+// });
 startGame = (cellsHorizontal, cellsVertical) => {
   const unitLengthX = width / cellsHorizontal;
   const unitLengthY = height / cellsVertical;
